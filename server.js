9570745329abc34d01cf2bb00e10af88bb3b1ab3
Timur <abdtimurrif@gmail.com>
2024-03-16 02:12:11 +0300

    3 laba

--- server.js
+++ server.js
@@ -1,37 +1,13 @@
-import fs from 'fs';
-import http from 'http';
-import {TaskManager} from './task-manager.js';
+import express from 'express';
+const app = express();
+app.use(express.json());
 
-const server = http.createServer((req, res) => {
-    fs.readFile('tasks.json', 'utf8', (err, data) => {
-        if (err) {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end("Error reading file.");
-            return;
-        }
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(data);
-    });
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
 });
 
-
-server.listen(3000, () => {
-
-    console.log("Server running at http://localhost:3000/");
-
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-const todoList = new TaskManager();
-todoList.loadTasks();
-
-todoList.on('taskAdded', (task) => {
-    console.log(`New task created: ${task.description}`);
-});
-
-todoList.on('taskRemoved', (task) => {
-    console.log(`Task delited: ${task.description}`);
-});
-
-// Добавление задачи
-todoList.addTask(12, "Новая задача", false);
-todoList.removeTask(12);
